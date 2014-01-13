37c88acc3eb3949ece45e05476ea6f2d48f0a509
Fabian Künzli <fabian.kuenzli@gmail.com>
2014-01-13 15:07:18 +0100

    Usecase class added to make objects persistent

---
+++ Usecase.java
@@ -0,0 +1,19 @@
+/*
+ * Written by Fabian Kuenzli
+ * University of Applied Sciences of Northwestern Switzerland, FHNW
+ * Computer Science, Software Engineering & Design
+ * fabian.kuenzli@gmail.com
+ * (c) 2014
+ */
+
+
+public class Usecase {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
