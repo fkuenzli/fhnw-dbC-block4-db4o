d96bf7e54f39a43a8c6666f928f9ce171693a0bc
Fabian Künzli <fabian.kuenzli@gmail.com>
2014-01-14 18:43:58 +0100

    base structure added to use database and execute usecase functions

--- Usecase.java
+++ Usecase.java
@@ -1,3 +1,7 @@
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+
 /*
  * Written by Fabian Kuenzli
  * University of Applied Sciences of Northwestern Switzerland, FHNW
@@ -14,6 +18,79 @@ public class Usecase {
 	 */
 	public static void main(String[] args) {
 		
+		ObjectContainer db = null;
+		db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "dbC_Projekt_4.db");
+		try {
+			
+			createTestData(db);
+			
+			/*
+			 * usecases
+			 * 
+			 * 
+			*/
+			
+		} catch(Exception e) {
+			
+			e.printStackTrace();
+		} finally {
+			if(db != null) {
+				db.close();
+			}			
+		}
+		
 	}
-
+	
+	/**
+	 * 
+	 * @param db
+	 */
+	public static void createTestData(ObjectContainer db) {
+		try {
+			
+			/*
+			 * do some database manipulations
+			 * ...
+			 * 
+			 * 
+			 * db.store(OBJECT);
+			 * 
+			*/
+			
+			db.commit();
+			
+		} catch(Exception e) {
+			db.rollback();
+			e.printStackTrace();
+		} finally {
+			
+		}
+		
+	}
+	
+	/**
+	 * 
+	 * @param db
+	 */
+	public static void usecase1(ObjectContainer db) {
+		try {
+			
+			/*
+			 * do some database manipulations
+			 * ...
+			 * 
+			 * 
+			 * db.store(OBJECT);
+			 * 
+			*/
+			
+			db.commit();
+			
+		} catch(Exception e) {
+			db.rollback();
+			e.printStackTrace();
+		} finally {
+			
+		}
+	}
 }
